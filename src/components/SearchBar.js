f0e7563b0ee04cf6cb123f3292b3bbd0580b0f82
Camilo <95107808+CamiloCardenasMesa@users.noreply.github.com>
2023-07-26 15:08:22 -0500

    feat: recommendations in comments added

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -32,4 +32,13 @@ HANDLING TEXT INPUT
 3) cuando se active el evento "onChange", obtener el valor del input
 4) tomar ese valor del input y usarlo para actualizar el estado
 5) pasar el estado al input como el valor (value) del prop.
+
+¿por qué?
+- Así podemos manejar el texto en el input usando el sistema de estado o "state system"
+- Cada vez que quedamos saber el valor actual de una entrada (input), podemos hacer referencia a la pieza de estado
+- Cada vez que queramos cambiar el valor dentro de la entrada (input), podemos actualizar esa pieza de estado (setTerm)
+- Cuando se manejen formularios nos referimos a él como entrada controlada "controlled inputs"
+
+
 */
+
